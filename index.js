24e96b0cca9c2a5ec3456b11a696e838fa53e921
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-06-24 16:32:36 +0530

    Adds filters

--- index.js
+++ index.js
@@ -3,7 +3,29 @@ const myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", [
     "$scope",
     ($scope) => {
-        $scope.message = "Hello everyone!";
-        $scope.coders = ["Arsh", "Ravi", "John", "Jake"];
+        $scope.message = "The Ninjas:";
+
+        $scope.ninjas = [
+            {
+                name: "Yoshi",
+                belt: "green",
+                price: 20,
+            },
+            {
+                name: "Crystal",
+                belt: "Yellow",
+                price: 30,
+            },
+            {
+                name: "Ryu",
+                belt: "orange",
+                price: 10,
+            },
+            {
+                name: "shaun",
+                belt: "black",
+                price: 40,
+            },
+        ];
     },
 ]);
